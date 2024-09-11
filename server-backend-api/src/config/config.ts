dc24304d533188b85596845c6dde70069d706580
Kamil Błoński <kblonski02@gmail.com>
2024-09-11 13:37:14 +0200

    next config and basic structure server.ts

---
+++ server-backend-api/src/config/config.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const ALLOWED_ORIGINS: string[] = (process.env.ALLOWED_ORIGINS || '').split(',').filter(Boolean);
+export const API_PORT : number = Number(process.env.API_PORT) ?? 3010;
+export const DATABASE_HOST : string = String(process.env.DATABASE_HOST) ?? 'localhost';
+export const DATABASE_PORT : number = Number(process.env.DATABASE_PORT) ?? 3306;
+export const DATABASE_USER : string = String(process.env.DATABASE_USER) ?? 'root';
+export const DATABASE_PASSWORD : string = String(process.env.DATABASE_PASSWORD) ?? '';
+
--- server-backend-api/src/server.ts
+++ server-backend-api/src/server.ts
@@ -1,1 +1,31 @@
-console.log("Hello from server.ts");
+import express from 'express';
+import cors from 'cors';
+import { ALLOWED_ORIGINS, API_PORT } from './config/config';
+
+const app = express();
+
+// Middleware CORS
+app.use(cors({
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+        if (origin && ALLOWED_ORIGINS.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'), false);
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    // allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use( express.json());
+app.use( express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg: "Server started at PORT " + API_PORT
+    })
+})
+
+app.listen(API_PORT, () => {
+    console.log('Server running on port ' + API_PORT);
+});
