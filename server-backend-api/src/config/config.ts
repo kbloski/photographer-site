5a71c4398d3a455a823217c12f40f2b04a246638
Kamil Błoński <kblonski02@gmail.com>
2024-09-11 13:43:42 +0200

    Add connect with database in db.js

--- server-backend-api/src/config/config.ts
+++ server-backend-api/src/config/config.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 export const ALLOWED_ORIGINS: string[] = (process.env.ALLOWED_ORIGINS || '').split(',').filter(Boolean);
 export const API_PORT : number = Number(process.env.API_PORT) ?? 3010;
+export const DATABASE_NAME : string = String(process.env.DATABASE_NAME) ?? 'photography';
 export const DATABASE_HOST : string = String(process.env.DATABASE_HOST) ?? 'localhost';
 export const DATABASE_PORT : number = Number(process.env.DATABASE_PORT) ?? 3306;
 export const DATABASE_USER : string = String(process.env.DATABASE_USER) ?? 'root';
---
+++ server-backend-api/src/utils/db.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+import { DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USER } from "../config/config";
+
+const sequelize = new Sequelize({
+    database: DATABASE_NAME,
+    dialect: 'mysql',
+    host: DATABASE_HOST,
+    username: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    port: DATABASE_PORT
+});
+
+sequelize.authenticate()
+.then( ()=> console.log(`Connected with database at port ${DATABASE_PORT}`))
+.catch( err => console.error("unable to connect to the database", err));
+
+export { sequelize };
