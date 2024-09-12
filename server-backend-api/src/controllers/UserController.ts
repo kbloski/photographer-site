9a9a16b5d524316a48f8b9d003979c90ad698de0
Kamil Błoński <kblonski02@gmail.com>
2024-09-13 00:22:21 +0200

    fix bug in UserController

--- server-backend-api/src/controllers/UserController.ts
+++ server-backend-api/src/controllers/UserController.ts
@@ -2,19 +2,18 @@ import { ModelStatic } from "sequelize";
 import { AbstractCrud } from "./abstractCrudController";
 import { UserModel } from "../models/UserModel";
 import { UserRoles, UserType } from "../types/UserType";
-import bcrypt from 'bcrypt';
-
+import bcrypt from "bcrypt";
 
 export class UserController extends AbstractCrud<UserModel> {
     constructor() {
         super(UserModel);
     }
-    
+
     // @ts-ignore
-    async create(data: UserType) : Promise<UserModel | null>{
+    async create(data: UserType): Promise<UserModel | null> {
         try {
             const salt = await bcrypt.genSalt(10);
-            data.password = await bcrypt.hash(data.password, salt);
+            data.password = await bcrypt.hash(String(data.password), salt);
 
             // @ts-ignore
             return await this.model.create(data);
@@ -24,11 +23,11 @@ export class UserController extends AbstractCrud<UserModel> {
         }
     }
 
-    async getByEmail ( email: string) : Promise<UserModel | null> {
-        return await this.model.findOne({where: {email}});
+    async getByEmail(email: string): Promise<UserModel | null> {
+        return await this.model.findOne({ where: { email } });
     }
 
-    async validPassword(password: string, userDb: UserModel) : Promise<boolean> {
-        return await bcrypt.compare(password, userDb.password )
+    async validPassword(password: string, userDb: UserModel): Promise<boolean> {
+        return await bcrypt.compare(password, userDb.password);
     }
 }
