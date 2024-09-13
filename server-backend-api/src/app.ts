9b3e18340ff239b245802e39bdcdc9bd2d7695d5
Kamil Błoński <kblonski02@gmail.com>
2024-09-13 16:38:18 +0200

    Enabled for alladdress cors

--- server-backend-api/src/app.ts
+++ server-backend-api/src/app.ts
@@ -9,17 +9,20 @@ const app = express();
 // Middleware CORS
 app.use(cors({
     origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-        if (origin && ALLOWED_ORIGINS.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'), false);
-        }
+        // Development -----
+        callback(null, true) 
+        // if (origin && ALLOWED_ORIGINS.includes(origin) || !origin) {
+        //     callback(null, true);
+        // } else {
+        //     callback(new Error('Not allowed by CORS'), false);
+        // }
     },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     // allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use( express.json());
+app.use( express.urlencoded());
+app.use( express.json() );
 app.use( express.static('./public'))
 
 // Trasy
