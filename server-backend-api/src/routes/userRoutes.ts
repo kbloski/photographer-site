8b5ab2bd1125239ad31ae3436017525834f838ba
Kamil Błoński <kblonski02@gmail.com>
2024-09-13 20:04:07 +0200

    Add post and patch userRoutes and create UserSchema with zodk

--- server-backend-api/src/routes/userRoutes.ts
+++ server-backend-api/src/routes/userRoutes.ts
@@ -3,6 +3,11 @@ import { apiUrlBuilderV1 } from "../services/ApiUrlBuilder";
 import { sendError, sendSuccess } from "../utils/responseUtils";
 import { userController } from "../controllers/controllers";
 import { UserType } from "../types/UserType";
+import { error } from "console";
+import { UserSchema } from "../schemas/UserSchema";
+import { z, ZodError } from "zod";
+import { generateZodErrorString } from "../utils/zodErrorsUtils";
+import { send } from "process";
 
 const router = express.Router();
 const resource = "user";
@@ -32,20 +37,45 @@ router.get(apiUrlBuilderV1.createUrlWithId(resource), async (req, res) => {
     }
 });
 
-router.post(apiUrlBuilderV1.createUrlWithId(resource), async (req, res) => {
-    // zapisz zasób na serwerze
+
+router.post(apiUrlBuilderV1.createUrlAdd(resource), async (req, res) => {
+        try {
+            const userData: Partial<Omit<UserType, 'id'>> = req.body;
+            UserSchema.parse(userData);
+            
+            const userDb = await userController.create( userData as UserType);
+
+            sendSuccess(req, res, 201, { created: true, user: userDb });
+        } catch (err) {
+            if (err instanceof z.ZodError) return sendError(req, res, 400, `Bad request: ${ generateZodErrorString(err) }`)
+            sendError(req, res);
+        }
 });
 
 router.patch(apiUrlBuilderV1.createUrlWithId(resource), async (req, res) => {
-    // Update zasobu
+    try {
+        const { id } = req.params;
+        if (!Number(id)) return sendError(req, res, 400, "Bad request");
+        
+        delete req.body.id;
+        const updatedUser : UserType = req.body;
+        UserSchema.partial().parse(updatedUser);
+
+        const updated = await userController.updateById(Number(id), updatedUser);
+        
+        sendSuccess(req, res, 200, { updated } );
+    } catch (err){
+        if (err instanceof z.ZodError) return sendError(req, res, 400, `Bad request: ${generateZodErrorString(err)}`);
+        sendError(req, res);
+    }
 });
 
 router.delete(apiUrlBuilderV1.createUrlWithId(resource), async (req, res) => {
     try {
         const { id } = req.params;
         if (!Number(id)) return sendError(req, res, 400, "Bad request");
-        const deleted : number = await userController.deleteById(Number(id));
-        sendSuccess(req, res, 200, { deleted: deleted})
+        const deleted: number = await userController.deleteById(Number(id));
+        sendSuccess(req, res, 200, { deleted: deleted });
     } catch (err) {
         console.error(err);
         sendError(req, res);
---
+++ server-backend-api/src/schemas/UserSchema.ts
@@ -0,0 +1,14 @@
+import { infer, z } from "zod";
+import { UserRoles } from "../types/UserType";
+
+export const UserSchema = z.object(
+     {
+        id: z.number().optional(),
+        role: z.nativeEnum(UserRoles).optional(),
+        username: z.string().min(2).max(64),
+        email: z.string().min(5).max(128).email(),
+        password: z.string().min(4).max(128),
+        phone: z.string().min(9).max(15).optional(),
+     }
+)
+
