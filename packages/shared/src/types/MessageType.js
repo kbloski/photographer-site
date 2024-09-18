a9a37ec3b922d45af847534735f2646d6bcc7ab3
Kamil Błoński <kblonski02@gmail.com>
2024-09-18 23:28:09 +0200

    Repair .gitignore in shared

--- packages/shared/src/types/MessageType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MessageStatus = void 0;
-var MessageStatus;
-(function (MessageStatus) {
-    MessageStatus["NEW"] = "new";
-    MessageStatus["READ"] = "read";
-    MessageStatus["ARCHIVED"] = "archived";
-})(MessageStatus || (exports.MessageStatus = MessageStatus = {}));
--- packages/shared/src/types/UserType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRoles = void 0;
-var UserRoles;
-(function (UserRoles) {
-    UserRoles["ADMIN"] = "admin";
-    UserRoles["CLIENT"] = "client";
-    // GUEST = 'guest'
-})(UserRoles || (exports.UserRoles = UserRoles = {}));
